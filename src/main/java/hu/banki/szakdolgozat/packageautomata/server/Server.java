a8fbca5b72869cbb704e498d21aa7fc261c17781
TothKristosz <112863872+TothKristosz@users.noreply.github.com>
2023-02-16 09:31:47 +0100

    Adatbázis kapcsolata kiépítése
    Adatbázis összekötése Java kóddal, localhoston

--- src/main/java/hu/banki/szakdolgozat/packageautomata/server/Server.java
+++ src/main/java/hu/banki/szakdolgozat/packageautomata/server/Server.java
@@ -4,20 +4,45 @@ import hu.banki.szakdolgozat.packageautomata.application.Main;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Server {
 
     private static final Logger logger = LoggerFactory.getLogger(Server.class);
 
     public void start() {
-
         logger.info("Server Started");
 
         //ide jon az uzleti logika
         //Logger beallitasa
         //Adatbazis felepitese basic SQL kod a teszt miatt
         //minta adatok feltöltésenek a kodja jon majd ide
-
         //Kliensrol kiadott utasítasok felgolgozasa
         //stb
+
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/packageautomata", "boss", ".[/Hl_YaZPynAvgk");
+
+            String sql = "INSERT INTO vdtb_city (ID, PostalCode, Name) VALUES (?, ?, ?)";
+            stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, 2);
+            stmt.setString(2, "2800");
+            stmt.setString(3, "Tatabánya");
+            int rows = stmt.executeUpdate();
+
+            System.out.println(rows + " row(s) inserted.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+
+        }
     }
 }
--- src/main/java/hu/banki/szakdolgozat/packageautomata/server/settings/Properties.java
+++ src/main/java/hu/banki/szakdolgozat/packageautomata/server/settings/Properties.java
@@ -8,12 +8,17 @@ public class Properties {
     //TODO: fv: property f�jl olvas�s
     //TODO: fv: map-eles
 
-    public static String MYSQL_CONNECTION_STRING = "jdbc:mysql://localhost:3306/database name";
-    //3306 lehet de a settingsben kiderül majd
+    public static String MYSQL_CONNECTION_STRING = "jdbc:mysql://localhost:3306/packageautomata"; //itt írjuk be az URL-t
+
+    //3306 lehet, de a settingsben kiderül majd
     //TODO adatokkkal feltolteni:
-    public static String MYSQL_SCHEMA = "something"; //Database name ide
-    public static String MYSQL_USERNAME = "username"; //basic username amivel tudok csatlakozni a databasehez
-    public static String MYSQL_PASSWORD = "password";//basic pass amivel tudok csatlakozni a databasehez
+    public static String MYSQL_SCHEMA = "packageautomata"; //Database name ide
+    public static String MYSQL_USERNAME = "boss"; //basic username amivel tudok csatlakozni a databasehez
+    public static String MYSQL_PASSWORD = ".[/Hl_YaZPynAvgk";//basic pass amivel tudok csatlakozni a databasehez
     public static int MYSQL_INSERT_MAX_ROWS = 1000;
+
+
+    //boss
+    //.[/Hl_YaZPynAvgk
 
 }
