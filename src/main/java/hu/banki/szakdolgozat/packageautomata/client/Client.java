e535cc6c4a9b17c8349ca63a312eb5b13d61a630
TothKristosz <112863872+TothKristosz@users.noreply.github.com>
2023-02-23 10:41:42 +0100

    Icon
    Átmeneti icon beállítása

--- src/main/java/hu/banki/szakdolgozat/packageautomata/client/Client.java
+++ src/main/java/hu/banki/szakdolgozat/packageautomata/client/Client.java
@@ -46,7 +46,7 @@ public class Client extends Application {
         stage.setTitle("Csomagautomata");
 
         //Windowsos alkalmazas ikonja
-        stage.getIcons().add(new Image("file:szakdoga_kosar.jpg"));
+        stage.getIcons().add(new Image("file:icon.png"));
 
         //Stage - alkalmazas, Scene a tartalom. Ossze kell rendelni oket
         stage.setScene(scene);
