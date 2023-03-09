8042d547e81a999035c865731233a846714aafa2
TothKristosz <112863872+TothKristosz@users.noreply.github.com>
2023-03-09 09:39:01 +0100

    Konstruktor visszaállítása
    Container classban visszaállítottam a konstruktort mert egy korábbi commit törölte azt

--- src/main/java/hu/banki/szakdolgozat/packageautomata/server/models/Container.java
+++ src/main/java/hu/banki/szakdolgozat/packageautomata/server/models/Container.java
@@ -1,5 +1,16 @@
 package hu.banki.szakdolgozat.packageautomata.server.models;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.StringProperty;
+
 public class Container {
+    //ID int
+    //AddressID int
+    //Size string
+
+    private IntegerProperty id;
+    private IntegerProperty addressID;
+    private StringProperty size;
+    
 
 }
